1c53d6f4564d4002d912b5b1640178b2d4fddeb1
Tran Hieu <wizard201366@gmail.com>
2021-05-31 00:01:31 +0700

    Fix logout url

--- PM-BE/pm-application/src/main/webapp/app/components/Dropdown/ProfileMenu/ProfileMenu.js
+++ PM-BE/pm-application/src/main/webapp/app/components/Dropdown/ProfileMenu/ProfileMenu.js
@@ -13,7 +13,7 @@ class ProfileMenu extends MaleficComponent {
             title: {type: String},
             avtImg: {type: String},
             id: {type: String},
-            logoutUrl: {type: String}
+            logout: {type: String}
         };
     }
     
@@ -34,12 +34,17 @@ class ProfileMenu extends MaleficComponent {
         super.connectedCallback();
     }
     
-    async logout() {
+    async signOut() {
         await store.dispatch(logout());
         const state = await store.getState();
-        this.logoutUrl = state.auth.logout;
-        if (this.logoutUrl) {
-            window.location.href = this.logoutUrl + '?redirect_uri=' + window.location.origin;
+        this.logout = state.auth.logout;
+        const logoutUrl = this.logout.logoutUrl;
+        const idToken = this.logout.idToken;
+        if (logoutUrl) {
+            // if Keycloak, logoutUrl has protocol/openid-connect in it
+            window.location.href = logoutUrl.includes('/protocol')
+                ? logoutUrl + '?redirect_uri=' + window.location.origin
+                : logoutUrl + '?id_token_hint=' + idToken + '&post_logout_redirect_uri=' + window.location.origin;
         }
     }
     
@@ -57,7 +62,7 @@ class ProfileMenu extends MaleficComponent {
                     <li><i class="fas fa-edit"></i><a href="edit-profile/personal">Edit Profile</a></li>
                     <li><i class="fas fa-users-cog"></i><a href="account">Account Settings</a></li>
                     <li><i class="fas fa-address-card"></i><a href="my-cv">My CV</a></li>
-                    <li><i class="fas fa-sign-out-alt"></i><a @click="${this.logout}">Sign Out</a></li>
+                    <li><i class="fas fa-sign-out-alt"></i><a style="cursor: pointer" @click="${this.signOut}">Sign Out</a></li>
                 </ul>
             </div>
         `;
